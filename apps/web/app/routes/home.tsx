a3aa3b1cf7ab9ca7ed3dc21b839fa7d06e7b2017
ETFairfax <20703554+ETFairfax@users.noreply.github.com>
2026-09-09 13:32:55 +0100

    feat: :sparkles: Show alert if the web application screen resolution < 1024x768.

--- apps/web/app/routes/home.tsx
+++ apps/web/app/routes/home.tsx
@@ -1,14 +1,26 @@
+import { Alert, AlertDescription, AlertTitle } from "@workspace/ui/components/alert"
 import { Button } from "@workspace/ui/components/button"
+import { InfoIcon } from "lucide-react"
 
 export default function Home() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
+    <div className="p-4">
+      <Alert variant="destructive" className="lg:hidden ">
+        <InfoIcon />
+        <AlertTitle>Screen size not supported</AlertTitle>
+        <AlertDescription>
+          This application is intended to be used on desktop. Minimum screen resolution: 1024x768.
+        </AlertDescription>          
+      </Alert>
+    
+      <div className="flex min-h-svh p-6">
+        <div className="hidden lg:flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+          <div>
+            <h1 className="font-medium">Project ready!</h1>
+            <p>You may now add components and start building.</p>
+            <p>We&apos;ve already added the button component for you.</p>
+            <Button className="mt-2">Button</Button>
+          </div>
         </div>
       </div>
     </div>
